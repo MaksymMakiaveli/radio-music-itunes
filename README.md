3e6c8f06230f5421b97234fd8e5869b02339e59c
MaksymMakiaveli <babiim97@gmail.com>
2021-07-04 02:41:51 +0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# https://itunesMyWeb.github.io/
+# <p align='center' >radio-music-iTunes</p>
+
+### _GitHub pages:_ https://itunesMyWeb.github.io/
+
+<br/>
+<br/>
+
+- HTML/SCSS
+- JavaScript
--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,32 @@
 // import file videoPlayer
-import {
-   videoPlayerInit
-} from './videoPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
 // import file radioPlayer
-import {
-   radioPlayerInit
-} from './radioPlayer.js';
+import { radioPlayerInit } from './radioPlayer.js';
 // import file musicPlayer
-import {
-   musicPlayerInit
-} from './musicPlayer.js';
-
-
-// переменные
+import { musicPlayerInit } from './musicPlayer.js';
 
 const playerBtn = document.querySelectorAll('.player-btn');
 const playerBlock = document.querySelectorAll('.player-block');
 const temp = document.querySelector('.temp');
 
 const deactivationPlayer = () => {
-   temp.style.display = 'none';
-   playerBtn.forEach(item => item.classList.remove('active'));
-   playerBlock.forEach(item => item.classList.remove('active'));
+  temp.style.display = 'none';
+  playerBtn.forEach((item) => item.classList.remove('active'));
+  playerBlock.forEach((item) => item.classList.remove('active'));
 
-
-   musicPlayerInit.stop();
-   videoPlayerInit.stop();
-   radioPlayerInit.stop();
+  musicPlayerInit.stop();
+  videoPlayerInit.stop();
+  radioPlayerInit.stop();
 };
 
+playerBtn.forEach((btn, i) =>
+  btn.addEventListener('click', () => {
+    deactivationPlayer();
+    btn.classList.add('active');
+    playerBlock[i].classList.add('active');
+  }),
+);
 
-playerBtn.forEach((btn, i) => btn.addEventListener('click', () => {
-   deactivationPlayer();
-   btn.classList.add('active');
-   playerBlock[i].classList.add('active');
-}));
-
-
-// call function
 videoPlayerInit();
 radioPlayerInit();
 musicPlayerInit();
